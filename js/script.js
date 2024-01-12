01083ab4fea7d86f981b11674fda9319b6ed5e35
Marc <2023_marc.cuzcano@iticbcn.cat>
2024-01-12 16:00:24 +0100

    Añadiendo funciones a los botones y mostrando por consola los datos de archivos json

--- js/script.js
+++ js/script.js
@@ -1,87 +1,133 @@
-			
-// POKEMONS
-
-let dades;
-/*
-
-// POKEMONS
-fetch("js/data/pokemon.json")
-.then((response) => response.json())
-.then((data) => {
-	dades = data.pokemon;		
-	
-	console.log(dades[0].name)
-	console.log(dades[1].name)
-
-});
-*/
-/*
-let iniciar = location.reload();
 
 function orderList(params) {
-	
+
 }
 function searchList(params) {
-	
-}
+	let buscarPokemon = prompt("Que elemento quieres buscar ?");
 
+}
+function iniciar(params) {
+	location.reload();
+
+}
 function calcMitjana(params) {
-	
+
 }
 
 function printList(params) {
-	
+
+
 }
-*/
-/*
-// MUNICIPIS
-fetch("js/data/municipis.json")
-.then((response) => response.json())
-.then((data) => {
-	dades = data.elements;		
-	
-	console.log(dades[1].municipi_nom)
-});
+
+// Array Multidimensional 
 
 
-*/
+function nuevaPartida() {
 
 
-/*
-// METEORITS
+}
+
+function reiniciarJuego() {
+	location.reload();
+}
+function ordenDescendente() {
+
+}
+function ordenAscendente() {
+
+}
+function medianaPeso() {
+
+}
+
+// Meteoritos
+
 fetch("js/data/earthMeteorites.json")
-.then((response) => response.json())
-.then((data) => {
-	dades = data;		
-	
-	console.log(dades)
-	console.log(dades[0].name)
-});
+	.then((response) => response.json())
+	.then((data) => {
+		dades = data;
+
+		let meteoritosArray = [];
+
+		dades.forEach((data) => {
+			meteoritosArray.push({
+				"Nombre": data.name
+			});
+		});
+
+		// Mostrar el array en forma de tabla
+		// console.table(meteoritosArray);
+	});
+
+// Array de Pokemons
+
+fetch("js/data/pokemon.json")
+	.then((response) => response.json())
+	.then((data) => {
+		dades = data.pokemon;
+
+		// Crear un array para almacenar la información de cada Pokémon
+		let pokemonArray = [];
+
+		// Utilizar forEach para iterar sobre cada Pokémon y añadirlo al array
+		dades.forEach((pokemon) => {
+			pokemonArray.push({
+				"Nombre": pokemon.name
+			});
+		});
+
+		// Mostrar el array en forma de tabla
+		// console.table(pokemonArray);
+	});
 
 
-// MOVIES
+// Array de peliculas
+
 fetch("js/data/movies.json")
-.then((response) => response.json())
-.then((data) => {
-	dades = data.movies;		
-	
-	console.log(dades)
-	console.log(dades[0].title)
-});
+	.then((response) => response.json())
+	.then((data) => {
+		dades = data.movies;
+
+		let moviesArray = [];
+
+		dades.forEach((movies) => {
+			moviesArray.push({
+				"Nombre": movies.title
+			});
+		});
+
+		// Mostrar el array en forma de tabla
+	//	 console.table(moviesArray);
+	});
+
+
+
+// Array de municipios
+fetch("js/data/municipis.json")
+	.then((response) => response.json())
+	.then((data) => {
+		dades = data.elements;
+
+		let municipisArray = [];
+
+		dades.forEach(dades => {
+			municipisArray.push({
+				"Nombre ": dades.municipi_nom
+			})
+		});
+		console.table(municipisArray);
+	});
+
+
+/*
+
+
+// Funciones asincronas
+async function getPeliculas(){
+	const response = await response.json();
+	peliculas = data.movies;
+	return data.movies
+};
+
 
 */
-
-// MOVIES
-fetch("js/data/movies.json")
-.then((response) => response.json())
-.then((data) => {
-	dades = data.movies;		
-	
-	console.log(dades)
-	//console.log(dades[0].title)
-	const iterador = dades.entries();
-    for (const entrada of iterador) {
-        const [name, valor] = entrada;
-        document.write(`<p>Índice: ${}, Valor: ${valor}</p>`);
-    }
-});
